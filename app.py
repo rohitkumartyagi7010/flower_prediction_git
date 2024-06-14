cfef4e889baccb02f5b3869ba4e876f180cebe30
Rohit <rohittyagi70101@gmail.com>
2024-06-14 15:53:36 +0530

    fith change

--- app.py
+++ app.py
@@ -18,4 +18,5 @@ def predict():
     return render_template("index.html", prediction_text = "The flower species is {}".format(prediction))
 
 if __name__ == "__main__":
-    flask_app.run(host = "0.0.0.0",port=8080,debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
